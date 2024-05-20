e20c95c6ac07ceb5b4571d893ffb10452b508cee
ephie <ephiewangyf@gmail.com>
2024-05-20 13:55:31 +1000

    plot

--- src/plot_ABTE.py
+++ src/plot_ABTE.py
@@ -7,13 +7,36 @@ Created on Sun May 19 20:13:31 2024
 """
 
 import numpy as np
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
+import itertools
 from consts import *
+from utils import *
 
 
-def plot_():
+def _get_df():
+    # adapter training 
+    dftest = pd.read_csv('model_ABTE_adapter/results/test_pred_lr3.0000000000000004e-05_epochs5_batch8.csv')
+    test_pred = dftest['Predicted']
+    
+    #load
+    data = pd.read_csv(TRAIN_DATA_PATH)
+    data_test = pd.read_csv(TEST_DATA_PATH)
+    
+    test_tags_real = [t.strip('][').split(', ') for t in data_test['Tags']]
+    test_tags_real = [[int(i) for i in t] for t in test_tags_real]
+    
+    test_pred = [t.strip('][').split(', ') for t in test_pred]
+    test_pred = [[int(i) for i in t] for t in test_pred]
+    
+    
+    ABTE_data = tag_to_word_df(data_test, 'gold terms', test_tags_real)
+    ABTE_data = tag_to_word_df(ABTE_data, 'pred terms', test_pred)
+    return ABTE_data
+
+def plot():
     # training loss and the validation loss for the fine-tuning and adapter cases, both with and without scheduler.
 
     lossABTE = np.loadtxt(
@@ -50,3 +73,56 @@ def plot_():
         os.makedirs('results_ABTE')
     fig.savefig('results_ABTE/loss_lr{:.5f}_epochs{}_batch{}.pdf'.format(
         lr, epochs, batch), dpi=300, bbox_inches='tight')
+    
+def report():
+    test_ATE = classification_report_read('model_ABTE/results/test_report_lr3.0000000000000004e-05_epochs5_batch8.csv')
+    train_ATE = classification_report_read('model_ABTE/results/train_report_lr3.0000000000000004e-05_epochs5_batch8.csv')
+    print_aligned(test_ATE, train_ATE, 'TEST FINE-TUNING', 'TRAIN FINE-TUNING')
+    
+    test_ATE_S = classification_report_read('model_ABTE_scheduler/results/test_report_lr3.0000000000000004e-05_epochs5_batch8.csv')
+    train_ATE_S = classification_report_read('model_ABTE_scheduler/results/train_report_lr3.0000000000000004e-05_epochs5_batch8.csv')
+    print_aligned(test_ATE_S, train_ATE_S, 'TEST FINE-TUNING + SCHEDULER', 'TRAIN FINE-TUNING + SCHEDULER')
+    
+    test_ATE_A = classification_report_read('model_ABTE_adapter/results/test_report_lr3.0000000000000004e-05_epochs5_batch8.csv')
+    train_ATE_A = classification_report_read('model_ABTE_adapter/results/train_report_lr3.0000000000000004e-05_epochs5_batch8.csv')
+    print_aligned(test_ATE_A, train_ATE_A, 'TEST ADAPTER', 'TRAIN ADAPTER')
+    
+    test_ATE_AS = classification_report_read('model_ABTE_adapter_scheduler/results/test_report_lr3.0000000000000004e-05_epochs5_batch8.csv')
+    train_ATE_AS = classification_report_read('model_ABTE_adapter_scheduler/results/train_report_lr3.0000000000000004e-05_epochs5_batch8.csv')
+    print_aligned(test_ATE_AS, train_ATE_AS, 'TEST ADAPTER + SCHEDULER', 'TRAIN ADAPTER + SCHEDULER')
+    
+
+    
+def word_cloud (data):
+    from wordcloud import WordCloud
+    wordcloud = WordCloud( collocations=False,
+                          background_color="cornflowerblue",
+                          colormap="magma",
+                          max_words=50).generate(data)
+
+    return wordcloud
+
+def target_predicted_wordcloud(targets, predicted, file_name):
+    
+    sns.set_theme(style='white', font_scale=2)
+    fig, ax = plt.subplots(1, 2, figsize=(22, 6))
+    ax[0].imshow(word_cloud(targets))
+    ax[0].axis("off")
+    ax[0].set_title("Target")
+    ax[1].imshow(word_cloud(predicted))
+    ax[1].axis("off")
+    ax[1].set_title("Predicted")
+    fig.savefig(file_name, dpi=300, bbox_inches='tight')
+    
+def gen_word_cloud():
+    ABTE_data = _get_df()
+    gold_terms = ABTE_data['gold terms'].values.flatten().tolist()
+    gold_terms = list(itertools.chain(*gold_terms))
+    
+    pred_terms = ABTE_data['pred terms'].values.flatten().tolist()
+    pred_terms = list(itertools.chain(*pred_terms))
+    target_predicted_wordcloud(' '.join(gold_terms), ' '.join(pred_terms), "results_ABTE/adapter_extracted_terms_wordcloud.pdf")
+
+def get_example(coun=10):
+    ABTE_data = _get_df()
+    return ABTE_data.head(20)
--- src/prediciton.sh
+++ src/prediciton.sh
@@ -3,8 +3,6 @@
 # Usage: ./train.sh ABTE
 
 
-batch=8
-epochs=5
 lr_schedule_values=("False", "True")
 adapter_values=("True" "False")
 
@@ -18,17 +16,11 @@ fi
 
 model=$1
 
-if [ "$model" == "ABTE" ]; then
-    lr=3*1e-5
-else
-    lr=1e-5
-fi
-
 
 for lr_schedule in "${lr_schedule_values[@]}"; do
     for adapter in "${adapter_values[@]}"; do
         echo "Running $model training with lr_schedule=$lr_schedule and adapter=$adapter"
-        python -m train_ABSA $model --batch="$batch" --epochs="$epochs" --lr="$lr" --lr_schedule="$lr_schedule" --adapter="$adapter"
+        python -m pred_ABSA $model --lr_schedule="$lr_schedule" --adapter="$adapter"
         echo "--------------------"
     done
 done
