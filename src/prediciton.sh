b2ae54f487300b0258abb8b3770f0f3fdc8bf760
Ephie <ephiewangyf@gmail.com>
2024-05-20 14:31:19 +1000

    ABTE plot (#2)
    * shell
    
    * add requirements
    
    * plot

--- src/prediciton.sh
+++ src/prediciton.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+
 # Usage: ./train.sh ABTE
 
 
-batch=8
-epochs=5
 lr_schedule_values=("False", "True")
 adapter_values=("True" "False")
 
@@ -17,17 +16,11 @@ fi
 
 model=$1
 
-if [ "$model" == "ABTE" ]; then
-    lr=3*1e-5
-else
-    lr=1e-5
-fi
-
 
 for lr_schedule in "${lr_schedule_values[@]}"; do
     for adapter in "${adapter_values[@]}"; do
         echo "Running $model training with lr_schedule=$lr_schedule and adapter=$adapter"
-        python -m train_ABSA $model --batch="$batch" --epochs="$epochs" --lr="$lr" --lr_schedule="$lr_schedule" --adapter="$adapter"
+        python -m pred_ABSA $model --lr_schedule="$lr_schedule" --adapter="$adapter"
         echo "--------------------"
     done
 done
