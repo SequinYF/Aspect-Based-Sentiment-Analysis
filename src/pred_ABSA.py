3b8ebc4938e3931d4ef0fb473f6efad08a73750b
ephie <ephiewangyf@gmail.com>
2024-05-27 10:56:56 +1000

    fix: RuntimeError Placeholder storage has not been allocated on MPS device

--- src/pred_ABSA.py
+++ src/pred_ABSA.py
@@ -18,7 +18,7 @@ import fire
 
 
 def run_ABSA_test_train(work_type, adapter, lr_schedule):
-    from consts import lr, epochs, batch, TEST_DATA_PATH, TRAIN_DATA_PATH
+    from consts import lr, epochs, batch, TEST_DATA_PATH, TRAIN_DATA_PATH, VAL_DATA_PATH
     
     if adapter:
         if lr_schedule:
@@ -32,7 +32,7 @@ def run_ABSA_test_train(work_type, adapter, lr_schedule):
             dir_name = "model_{}".format(work_type)
 
     # load
-    data = pd.read_csv(TRAIN_DATA_PATH)
+    data = pd.read_csv(VAL_DATA_PATH)
     data_test = pd.read_csv(TEST_DATA_PATH)
 
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
@@ -56,23 +56,25 @@ def run_ABSA_test_train(work_type, adapter, lr_schedule):
         model_path = dir_name+'/model_lr3.0000000000000004e-05_epochs2_batch8.pkl'
     else:
         raise Exception('wrong work type, must be ABSA or ABTE')
-        
-    print(model_path)
-    test_accuracy, test_report = modelABSA.test(
-        data_test, load_model=model_path, device=DEVICE)
-    test_pred, test_pol = modelABSA.predict_batch(
-        data_test, load_model=model_path, device=DEVICE)
-
-    train_accuracy, train_report = modelABSA.test(
-        data, load_model=model_path, device=DEVICE)
-    train_pred, train_pol = modelABSA.predict_batch(
-        data, load_model=model_path, device=DEVICE)
-
+    
     # save results
     if not os.path.exists(dir_name+'/results'):
         os.makedirs(dir_name+'/results')
 
-    # report
+    print(model_path)
+    test_accuracy, test_report = modelABSA.test(
+        data_test, load_model=model_path, device=DEVICE)
+    train_accuracy, train_report = modelABSA.test(
+        data, load_model=model_path, device=DEVICE)
+    
+    #accuracy
+    test_accuracy = np.array(test_accuracy)
+    train_accuracy = np.array(train_accuracy)
+
+    with open(dir_name+'/results/test_accuracy_lr{}_epochs{}_batch{}.csv'.format(lr, epochs, batch), 'w') as f:
+         f.write(str(test_accuracy))
+
+        #report
     with open(dir_name+'/results/test_report_lr{}_epochs{}_batch{}.csv'.format(lr, epochs, batch), 'w') as f:
         for r in test_report.split('\n'):
             f.write(r + '\n')
@@ -80,6 +82,12 @@ def run_ABSA_test_train(work_type, adapter, lr_schedule):
     with open(dir_name+'/results/train_report_lr{}_epochs{}_batch{}.csv'.format(lr, epochs, batch), 'w') as f:
         for r in train_report.split('\n'):
             f.write(r + '\n')
+
+    DEVICE = 'cpu' #otherwise 'RuntimeError: Placeholder storage has not been allocated on MPS device!' in mac
+    train_pred, train_pol = modelABSA.predict_batch(
+        data, load_model=model_path, device=DEVICE)
+    test_pred, test_pol = modelABSA.predict_batch(
+        data_test, load_model=model_path, device=DEVICE)
 
     # predictions
     data_test['Predicted'] = test_pred
@@ -92,12 +100,6 @@ def run_ABSA_test_train(work_type, adapter, lr_schedule):
     data.to_csv(dir_name+'/results/train_pred_lr{}_epochs{}_batch{}.csv'.format(lr,
                 epochs, batch), index=False)
 
-    # accuracy
-    test_accuracy = np.array(test_accuracy)
-    train_accuracy = np.array(train_accuracy)
-
-    with open(dir_name+'/results/test_accuracy_lr{}_epochs{}_batch{}.csv'.format(lr, epochs, batch), 'w') as f:
-        f.write(str(test_accuracy))
     with open(dir_name+'/results/train_accuracy_lr{}_epochs{}_batch{}.csv'.format(lr, epochs, batch), 'w') as f:
         f.write(str(train_accuracy))
 
